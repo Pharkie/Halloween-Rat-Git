f1982bd883df5ea4679bd0a2a21af90a652bef88
Adam Knowles <1413836+Pharkie@users.noreply.github.com>
2023-10-22 02:19:06 +0100

    seems to work if Pico FS doesn't crash all the time?

--- code.py
+++ code.py
@@ -1,14 +1,17 @@
 """
-CircuitPython I2S MP3 playback example.
-Plays a single MP3 once.
+MP3 player using the I2S bus.
 """
 import board
 import audiomp3
 import audiobusio
+import random
 
 audio = audiobusio.I2SOut(board.GP0, board.GP1, board.GP2)
 
-mp3 = audiomp3.MP3Decoder(open("cardiff.mp3", "rb"))
+mp3s = ["cardiff.mp3", "standup-low.mp3", "slow.mp3"]
+mp3_file = random.choice(mp3s)
+
+mp3 = audiomp3.MP3Decoder(open(mp3_file, "rb"))
 
 audio.play(mp3)
 while audio.playing:
