0b16ec7acfc19e0fd9b8ee464006748d46fb3afb
Adam Knowles <1413836+Pharkie@users.noreply.github.com>
2023-10-23 21:23:21 +0100

    impoved code2

--- code.py
+++ code.py
@@ -5,16 +5,43 @@ import board
 import audiomp3
 import audiobusio
 import random
+import os
+import time
 
-audio = audiobusio.I2SOut(board.GP0, board.GP1, board.GP2)
+def main():
+    audio = audiobusio.I2SOut(board.GP0, board.GP1, board.GP2)
 
-mp3s = ["cardiff.mp3", "standup-low.mp3", "slow.mp3"]
-mp3_file = random.choice(mp3s)
+    folder_path = "/mp3"
+    mp3s = []
 
-mp3 = audiomp3.MP3Decoder(open(mp3_file, "rb"))
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".mp3"):
+            mp3s.append(filename)
 
-audio.play(mp3)
-while audio.playing:
-    pass
+    if not mp3s:
+        print("No mp3 files found in /mp3 directory")
+        return
 
-print("Done playing!")
+    last_mp3 = None
+    mp3_file = None
+
+    try:
+        while True:
+            # Select a random mp3 file that is different from the last one played
+            while mp3_file == last_mp3:
+                mp3_file = random.choice(mp3s)
+            last_mp3 = mp3_file
+
+            # Load and play the selected mp3 file
+            mp3 = audiomp3.MP3Decoder(open(folder_path + os.sep + mp3_file, "rb"))
+            audio.play(mp3)
+
+            # Wait for a random amount of time between 5 and 10 seconds before playing the next mp3
+            delay = random.randint(10, 20)
+            time.sleep(delay)
+
+    except KeyboardInterrupt:
+        print("Program stopped by user")
+
+if __name__ == "__main__":
+    main()
